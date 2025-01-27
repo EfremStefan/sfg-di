efd690419ceb3e9cfe90f3cc1b9120df756e7644
EfremStefan <st.efrem98@gmail.com>
2025-01-27 19:11:30 +0200

    test: test constructor DI, Good practice but without Spring annotations

---
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingsService;
+
+public class ConstructorInjectedController {
+
+    private final GreetingsService greetingsService;
+
+    public ConstructorInjectedController(GreetingsService greetingsService) {
+        this.greetingsService = greetingsService;
+    }
+
+    public String getGreeting(){
+        return greetingsService.sayGreeting();
+    }
+}
